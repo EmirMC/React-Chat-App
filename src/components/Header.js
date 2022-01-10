90efc5aa3e34386c6d466662e430dd7388e58592
EmirMC <emircamli59@gmail.com>
2022-01-10 20:37:30 +0300

    Edit Style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div>
+        <div className="w-full border-b-2 border-indigo-300 bg-indigo-600 text-white px-4 h-14 items-center flex flex-row">
             Header
         </div>
     )
